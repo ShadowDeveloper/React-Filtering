b74fbe7dab6ec717660110f457621be5519c15d4
shadowdeveloper <ro_web1@hotmail.com>
2017-04-25 22:51:05 -0300

    create card component

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,14 +1,21 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+
 import Titulo from './titulo/titulo';
 import Navbar from './navbar/navbar';
+import Cartao from './cartao/cartao';
 
 
 let App = (
   <div>
-    <Navbar titulo="React Logo" navcolor="green" />
+    <Navbar titulo="React Logo" navcolor="blue" />
     <div className="container">
       <Titulo />
+      <div className="row">
+        <div className="col m4">
+          <Cartao />
+        </div>
+      </div>
     </div>
   </div>
 );
---
+++ src/js/cartao/cartao.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Cartao extends React.Component{
+  render(){
+    return(
+      <div className="card">
+        <div className="card-image waves-effect waves-block waves-light">
+          <img className="activator" src="http://materializecss.com/images/office.jpg" />
+        </div>
+        <div className="card-content">
+          <span className="card-title activator grey-text text-darken-4">Titulo<i className="material-icons right">more_vert</i></span>
+          <p>Descricao</p>
+        </div>
+        <div className="card-action">
+          <a href="#">Ver mais</a>
+        </div>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4">Sobre<i className="material-icons right">Fechar</i></span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio perspiciatis natus ex sunt dolorem voluptatibus impedit.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cartao;
