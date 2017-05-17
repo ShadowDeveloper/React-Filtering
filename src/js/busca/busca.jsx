dd7ebfbce79a9e92ff1b4553e9c795473d435e1a
Rodrigo Souza <ro_web1@hotmail.com>
2017-05-17 11:41:28 -0300

    search prodcuts finished

--- src/js/busca/busca.jsx
+++ src/js/busca/busca.jsx
@@ -5,20 +5,28 @@ class Busca extends React.Component {
     super(props);
     this.state = { active: "active" }
     this.formUpdateSearch = this.formUpdateSearch.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
   }
  
   formUpdateSearch(event){
     this.props.updateSearch(event);
   }
 
+
+  onSubmitForm(event){
+    this.props.onSubmit(event);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmitForm}>
         <div className="input-field">
           <label htmlFor="">Busca</label>
           <input type="text" onChange={this.formUpdateSearch} placeholder="Digite sua busca" value={this.props.busca} />
         </div>
         <p>{this.props.busca}</p>
+        <button>Buscar</button>
       </form>
     );
   }
--- src/js/cartao/lista-cartao.jsx
+++ src/js/cartao/lista-cartao.jsx
@@ -9,10 +9,36 @@ class ListCards extends React.Component {
     super(props);
     this.state = {
       clicks: 0,
-      search:''
+      search: '',
+      dataList: [],
+      dataServerList:[]
     };
     this.addClick = this.addClick.bind(this);
     this.updateSearch = this.updateSearch.bind(this);
+    this.onSubmitList = this.onSubmitList.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataList: [
+        { titulo: 'Title 1', descricao: 'Descricao 1', detalhe: 'Rodrigo Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 2', descricao: 'Descricao 2', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 3', descricao: 'Descricao 3', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 4', descricao: 'Descricao 4', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 5', descricao: 'Descricao 5', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 6', descricao: 'Descricao 6', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 7', descricao: 'Descricao 7', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' }
+      ],
+      dataServerList: [
+        { titulo: 'Title 1', descricao: 'Descricao 1', detalhe: 'Rodrigo Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 2', descricao: 'Descricao 2', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 3', descricao: 'Descricao 3', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 4', descricao: 'Descricao 4', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 5', descricao: 'Descricao 5', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 6', descricao: 'Descricao 6', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
+        { titulo: 'Title 7', descricao: 'Descricao 7', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' }
+      ]
+    })
   }
 
   addClick() {
@@ -25,21 +51,34 @@ class ListCards extends React.Component {
     console.log("ok");
   }
 
-  updateSearch(event){
-    this.setState({search: event.target.value});
+  updateSearch(event) {
+    this.setState({ search: event.target.value });
+    if(event.target.value == ""){
+      this.setState({
+        dataList: this.state.dataServerList
+      })
+    }
+  }
+
+  onSubmitList(event) {
+    console.log(this.state.search);
+    let userSearch = this.state.search;
+    let userData = this.state.dataServerList;
+
+    let newList = userData.filter(function(item){
+      if(item.titulo.toUpperCase().indexOf(userSearch.toUpperCase()) > -1 || item.descricao.toUpperCase().indexOf(userSearch.toUpperCase()) > -1 || item.detalhe.toUpperCase().indexOf(userSearch.toUpperCase()) > -1){
+        return item;
+      }
+    });
+
+    this.setState({
+      dataList: newList 
+    })
+
   }
 
   render() {
-    let noticias = [
-      { titulo: 'Title 1', descricao: 'Descricao 1', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-      { titulo: 'Title 2', descricao: 'Descricao 2', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-      { titulo: 'Title 3', descricao: 'Descricao 3', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-      { titulo: 'Title 4', descricao: 'Descricao 4', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-      { titulo: 'Title 5', descricao: 'Descricao 5', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-      { titulo: 'Title 6', descricao: 'Descricao 6', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-      { titulo: 'Title 7', descricao: 'Descricao 7', detalhe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi alias omnis nisi consectetur, cum harum ad sed vero magni repellendus, incidunt accusantium laboriosam officiis numquam molestias distinctio, nesciunt, facere quo!', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' }
-    ];
-
+    let noticias = this.state.dataList;
     let aux = [];
     let novalista = [];
 
@@ -77,7 +116,7 @@ class ListCards extends React.Component {
 
     return (
       <div>
-        <Busca updateSearch = {this.updateSearch} busca = {this.state.search}/>
+        <Busca updateSearch={this.updateSearch} busca={this.state.search} onSubmit={this.onSubmitList} />
         <p>Quantidade de clicks: {this.state.clicks}</p>
         {line}
       </div>
